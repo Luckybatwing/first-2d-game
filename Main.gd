a397efa7033e082f997d29393e8adb5e9e003313
Sir Luck <63381079+Luckybatwing@users.noreply.github.com>
2022-05-15 03:07:23 -0400

    Add background, music, & start shortcut

--- Main.gd
+++ Main.gd
@@ -12,6 +12,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
 func new_game():
@@ -21,6 +23,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_StartTimer_timeout():
